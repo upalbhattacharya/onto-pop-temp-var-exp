b88c50a2c6cbce13c4da767cb63374cf23210e1e
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-07-30 13:51:32 +0200

    code/src/onto_pop_temp_var_exp/definitions.py

--- code/src/onto_pop_temp_var_exp/definitions.py
+++ code/src/onto_pop_temp_var_exp/definitions.py
@@ -2,3 +2,6 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).absolute().parent.parent
+DATA_DIR = ROOT_DIR / "data"
+RESULTS_DIR = ROOT_DIR / "results"
+RUN_ARGS_DIR = ROOT_DIR / "run_args"

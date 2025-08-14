4b688d59a7c218cc1f7b5517f4e427e15ee00c43
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-08-14 15:58:07 +0200

    Add RunArguments. Add Batch files

--- code/src/onto_pop_temp_var_exp/definitions.py
+++ code/src/onto_pop_temp_var_exp/definitions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).absolute().parent.parent
+ROOT_DIR = Path(__file__).absolute().parent.parent.parent
 DATA_DIR = ROOT_DIR / "data"
 RESULTS_DIR = ROOT_DIR / "results"
 RUN_ARGS_DIR = ROOT_DIR / "run_args"

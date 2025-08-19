d1be9d555f4bd130d62b3e80b1928e00867a1502
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-08-19 15:01:06 +0200

    code/src/onto_pop_temp_var_exp/analysis/temp_analysis.py

--- code/src/onto_pop_temp_var_exp/analysis/temp_analysis.py
+++ code/src/onto_pop_temp_var_exp/analysis/temp_analysis.py
@@ -8,4 +8,6 @@ import polars as pl
 
 
 def hypothesis_1(preds: dict[list[pl.DataFrame]]) -> None:
-    
+    """Compute and plot the average number of directly-asserted
+    concepts predicted for each query"""
+    # TODO: Improve docstring
